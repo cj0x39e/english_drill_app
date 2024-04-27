04a207ab249ba89a9f83d6db376aff53620657b2
cj0x39e <taozhi1028@gmail.com>
2024-04-27 16:31:02 +0800

    Using dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData.dark(),
         theme: ThemeData(
           // This is the theme of your application.
           //
